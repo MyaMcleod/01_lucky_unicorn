9a5a980c8e9ab10d90f38743e1019e0c762665a3
mcleod.mya <mcleod.mya@awatapu.school.nz>
2021-07-05 12:02:08 +1200

    Commit

---
+++ 00_sandbox.py
@@ -0,0 +1,8 @@
+#ask user for number
+get_number = int(input("Choose a number!"))
+
+#Times by five
+times_number = get_number * 5
+
+#Print result
+print("{} times by five equals {}".format(get_number, times_number))
