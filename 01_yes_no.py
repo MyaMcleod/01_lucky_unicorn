d9b6e241677a8eb86b07deff6cf812e5e43ab47c
mcleod.mya <mcleod.mya@awatapu.school.nz>
2021-07-27 14:22:52 +1200

    //

--- 01_yes_no.py
+++ 01_yes_no.py
@@ -5,8 +5,12 @@ show_instructions = input("Have you played before?").lower()
 if show_instructions == "yes" or show_instructions == "y", show_instructions == "Yes":
     print("Program continues!")
 
+#if they say No, output 'program  contiune'
+if show_instructions == "no" or show_instructions == "n", show_instructions == "No":
+    print("Display the instructions!")
+
 #if they say other, output 'Please answer the question'
-if show_instructions == "maybe":
+elif show_instructions == "maybe":
     print("Please answer the question with yes or no!")
 
 #if they say no, output 'display instructions'
