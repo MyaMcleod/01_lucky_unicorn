3812993dd2f0612caf7c1999c7b2d7fb6bf344f5
mcleod.mya <mcleod.mya@awatapu.school.nz>
2021-07-30 10:55:03 +1200

    lj

--- how_much_101.py
+++ how_much_101.py
@@ -1,14 +1,15 @@
 
-error = "Please enter a whole number between 1 and 10"
+error = "Please enter a whole number between 1 and 10\n"
 
 valid = False
 while not valid:
+    try:
+        response_1 = int(input("How much would you "
+                             "like to play with?"))
 
-    response = int(input("How much would you "
-                         "like to play with?"))
-
-if 0 < response <= 10 :
-    print("You have asked to play"
-          "with ${}".format(response))
-else:
-    print(error)
+        if 0 < response_1 <= 10:
+            print("You have asked to play with ${}".format(response_1))
+        else:
+            print(error)
+    execpt ValueError:
+        print(error)
